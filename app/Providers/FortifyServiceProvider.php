334a22661d0f3439263ae954d6a3b21967a5fcf9
steekam <waynewanyee@gmail.com>
2021-04-24 16:48:34 +0300

    feat: enabled email verification

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -36,6 +36,10 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
+        Fortify::verifyEmailView(function() {
+            return view('auth.verify-email');
+        });
+
         RateLimiter::for('login', function (Request $request) {
             return Limit::perMinute(5)->by($request->email.$request->ip());
         });
